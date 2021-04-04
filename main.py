7e8d309fd2cca5249f1253530a3b3487e3b7e7a5
Yujie Wang <yujiewang@berkeley.edu>
2021-04-04 15:12:56 -0400

    fixed minor datatype error

--- main.py
+++ main.py
@@ -105,8 +105,8 @@ def main():
     train_losses = []
     for epoch in range(n_epochs):
         train_loss = train(train_loader, model, criterion, optimizer, epoch)
-        train_losses.append(train_loss.data)
-        print("TOTAL LOSS FOR EPOCH {}: {}".format(epoch, train_loss.data[0]), end = '\r')
+        train_losses.append(train_loss.item())
+        print("TOTAL LOSS FOR EPOCH {}: {}".format(epoch, train_loss.item()), end = '\r')
         if epoch % 25 == 0:
             save_checkpoint(model.state_dict(), False, filename = 'epoch{}.pth.tar'.format(epoch))
     # TODO: remove, per Adham's request
